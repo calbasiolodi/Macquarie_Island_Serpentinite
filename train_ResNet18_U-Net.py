1fd9413c5f51b771a842bbbf1bf91c6ab0010c06
Cesare Lodi-Cusani <c.albasiolodi@gmail.com>
2026-09-11 10:53:29 +1000

    Create train_ResNet18_U-Net.py

---
+++ train_ResNet18_U-Net.py
@@ -0,0 +1,4 @@
+"""These are imports of essential packages to train the PyTorch model that is saved to a .pth (path) file, that is re-usable and more importantly can be fine-tuned.
+"""
+import torch
+import torch.nn as nn
